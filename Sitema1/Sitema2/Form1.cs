2af85c03dfa4064f744a79607d5ea1e2ba5936a0
lean1000 <166569100+lean1000@users.noreply.github.com>
2025-01-27 08:13:03 -0300

    Add files via upload

---
+++ Sitema1/Sitema2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sitema2
+{
+    public partial class Form1 : Form
+    {
+        bool VerSenhaTxt = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (VerSenhaTxt == false)
+            {
+                textBoxSenha.PasswordChar = '\0';
+                VerSenhaTxt = true;
+                buttonMostrarSenha.Text = "Ocultar Senha";
+            }
+            else
+            {
+                textBoxSenha.PasswordChar = '*';
+                VerSenhaTxt = false;
+                buttonMostrarSenha.Text = "Mostrar Senha";
+            }
+        }
+
+        private void buttonLimparCampos_Click(object sender, EventArgs e)
+        {
+            textBoxUsuario.Text = "";
+            textBoxSenha.Text = "";
+            textBoxUsuario.Focus();
+        }
+    }
+}
